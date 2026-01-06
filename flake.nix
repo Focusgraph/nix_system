e3f87be293fab4995cd587e21dbdb16261f87b3a
focusgraph <ruben.ledesma.go@protonmail.com>
2026-01-06 20:18:08 +0100

    home-manager setup

--- flake.nix
+++ flake.nix
@@ -4,13 +4,23 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
     nix-flatpak.url = "github:gmodena/nix-flatpak/?ref=latest";
+    home-manager.url = "github:nix-community/home-manager/release-25.11";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, nix-flatpak }: {
+  outputs = { self, nixpkgs, nix-flatpak, home-manager }:
+  {
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
       modules = [
+        ./configuration.nix
         nix-flatpak.nixosModules.nix-flatpak
-        ./configuration.nix 
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.nixy = ./home.nix;
+        }
       ];
     };
   };
