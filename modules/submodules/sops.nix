2c81564fd2a7f083e83d01a56fa8a725ec4965ed
Focusgraph <ruben.ledesma.go@protonmail.com>
2026-03-22 21:06:06 +0100

    regenerate secrets

--- modules/submodules/sops.nix
+++ modules/submodules/sops.nix
@@ -3,7 +3,6 @@
     defaultSopsFile = ../../secrets/secrets.yaml;
     age.sshKeyPaths = [ "/home/nixy/.ssh/key" ];
     age.keyFile = "/home/nixy/.config/sops/age/keys.txt";
-    # age.generateKey = true;
     secrets."nixy_password" = {};
     secrets."nixy_password".neededForUsers = true;
   };
