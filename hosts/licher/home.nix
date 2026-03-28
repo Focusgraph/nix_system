5555e94729d0b842cb635a3db9da6dea51d0bd3a
niridium <niridium@tuta.io>
2026-03-28 12:43:00 +0100

    modified:   hosts/licher/home.nix

--- hosts/licher/home.nix
+++ hosts/licher/home.nix
@@ -3,6 +3,8 @@
   home.stateVersion = "25.11";
   imports = [
     ../../home-modules
+    ../../home-modules/firefox.nix
+    ../../home-modules/keepassxc.nix
     inputs.noctalia.homeModules.default # To check if noctalia is enabled
   ];
 }
