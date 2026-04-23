ea1513b167ec176a22136ea14cc31bc6e1aac54e
niridium <niridium@tuta.io>
2026-04-23 19:28:04 +0200

    fix: redundant imports when both ghostty and noctalia are enabled

--- home/hosts/licher.nix
+++ home/hosts/licher.nix
@@ -1,7 +1,9 @@
+{ inputs, ... }:
 {
   home.stateVersion = "25.11";
   imports = [
     ../core
     ../tui
+    inputs.noctalia.homeModules.default
   ];
 }
--- home/tui/ghostty.nix
+++ home/tui/ghostty.nix
@@ -5,9 +5,7 @@
   ...
 }:
 {
-  imports = [
-    inputs.noctalia.homeModules.default
-  ];
+
   programs.ghostty = {
     enable = true;
     settings.theme = lib.mkIf config.programs.noctalia-shell.enable "noctalia";
