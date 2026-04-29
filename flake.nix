ea13ef753e07cd54cbafc84b4ee23787c771d7b8
niridium <niridium@tuta.io>
2026-04-29 23:30:44 +0200

    refactor: restructure flake inputs and simplify flake.nix layout

--- flake.nix
+++ flake.nix
@@ -1,46 +1,59 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    agenix.url = "github:ryantm/agenix";
-    arkenfox.url = "github:dwarfmaster/arkenfox-nixos";
-    direnv-instant.url = "github:Mic92/direnv-instant";
-    impermanence = {
-      url = "github:nix-community/impermanence";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+
+    # Infrastructure
     disko = {
       url = "github:nix-community/disko/latest";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    nix-index-database = {
-      url = "github:nix-community/nix-index-database";
+    impermanence = {
+      url = "github:nix-community/impermanence";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    # User Environment
     home-manager = {
       url = "github:nix-community/home-manager/";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    niri = {
-      url = "github:sodiboo/niri-flake";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     noctalia = {
       url = "github:noctalia-dev/noctalia-shell/v4.7.6";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    niri = {
+      url = "github:sodiboo/niri-flake";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    # System Tools
+    nix-index-database = {
+      url = "github:nix-community/nix-index-database";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    # Security
+    agenix.url = "github:ryantm/agenix";
+
+    # Network/Browser
+    arkenfox.url = "github:dwarfmaster/arkenfox-nixos";
+
+    # Utilities
+    direnv-instant.url = "github:Mic92/direnv-instant";
   };
+
   outputs =
     inputs@{
-      nix-index-database,
-      direnv-instant,
-      home-manager,
-      impermanence,
-      arkenfox,
-      noctalia,
       nixpkgs,
-      agenix,
       disko,
+      impermanence,
+      home-manager,
+      noctalia,
       niri,
+      nix-index-database,
+      agenix,
+      arkenfox,
+      direnv-instant,
       self,
       ...
     }:
@@ -59,26 +72,20 @@
             ];
           };
       };
+
       nixosConfigurations = {
         vega =
           let
             username = "nixy";
-            hostname = "vega";
-            swapspace = 8;
-            systemdisk = "/dev/nvme0n1";
-            specialArgs = {
-              inherit
-                inputs
-                username
-                hostname
-                swapspace
-                systemdisk
-                ;
-            };
           in
           nixpkgs.lib.nixosSystem {
-            inherit specialArgs;
             system = "x86_64-linux";
+            specialArgs = {
+              inherit inputs username;
+              hostname = "vega";
+              swapspace = 8;
+              systemdisk = "/dev/nvme0n1";
+            };
             modules = [
               ./hosts/vega
               disko.nixosModules.disko
@@ -92,35 +99,25 @@
                 home-manager.extraSpecialArgs = { inherit inputs; };
                 home-manager.backupFileExtension = "bkp";
                 nixpkgs.overlays = [ niri.overlays.niri ];
-                age.identityPaths = [ "/home/${username}/.ssh/key" ];
+                age.identityPaths = [ "/home/nixy/.ssh/key" ];
               }
             ];
           };
         licher =
           let
             username = "nixy";
-            hostname = "licher";
-            swapspace = 8;
-            systemdisk = "/dev/sdc";
-            serverdirectory = "/storage";
-            virtual_display = "amdgpu.virtual_display=0000:0c:00.0,1";
-            interface = "enp9s0";
-            specialArgs = {
-              inherit
-                inputs
-                username
-                hostname
-                swapspace
-                systemdisk
-                serverdirectory
-                virtual_display
-                interface
-                ;
-            };
           in
           nixpkgs.lib.nixosSystem {
-            inherit specialArgs;
             system = "x86_64-linux";
+            specialArgs = {
+              inherit inputs username;
+              hostname = "licher";
+              swapspace = 8;
+              systemdisk = "/dev/sdc";
+              serverdirectory = "/storage";
+              virtual_display = "amdgpu.virtual_display=0000:0c:00.0,1";
+              interface = "enp9s0";
+            };
             modules = [
               ./hosts/licher
               disko.nixosModules.disko
